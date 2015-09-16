d82cbe77e1b9587853ca12b5f1a873b27b89dc68
Joseph Bleau <joseph.bleau@bestbuy.com>
2015-09-16 00:51:10 -0400

    Begin timer update loop thread when the plugin is enabled and stop it when the plugin is disabled.

--- src/main/java/com/josephbleau/bukkit/timer/TimerPlugin.java
+++ src/main/java/com/josephbleau/bukkit/timer/TimerPlugin.java
@@ -3,13 +3,34 @@ package com.josephbleau.bukkit.timer;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TimerPlugin extends JavaPlugin {
+    private TimerManager timerManager = new TimerManager();
+
+    private boolean stopTimerThread = false;
+
+    private Thread timerUpdateThread = new Thread(new Runnable() {
+        public void run() {
+            while(!stopTimerThread) {
+                timerManager.updateTimers();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    getLogger().severe("Error in timer update thread: " + e.getMessage());
+                    return;
+                }
+            }
+        }
+    });
+
     @Override
     public void onEnable() {
         getLogger().info("Timer plugin is enabled.");
+        stopTimerThread = false;
+        timerUpdateThread.start();
     }
 
     @Override
     public void onDisable() {
         getLogger().info("Timer plugin was disabled.");
+        stopTimerThread = true;
     }
 }
